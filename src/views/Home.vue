d66bdeff3699b248aab13f288c1b05442583afdd
marshall chen <marshallchan666@outlook.com>
2021-06-12 19:34:58 +0800

    redirect , back and go, difference between  and

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
-
+  <h1>Homepage</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio cupiditate quod similique deserunt dicta sequi eveniet laborum aperiam voluptate nesciunt, fugiat assumenda aspernatur consequatur nostrum? Placeat saepe laboriosam ab deserunt.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quisquam odio vitae? Quaerat quae minima eligendi nam, natus veniam necessitatibus, magnam ab quam sint similique in quos rerum cupiditate id.</p>
 </template>
 
 <script>
