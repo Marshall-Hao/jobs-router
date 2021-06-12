cc3d271722a09332e3ca9c6e3baa1275d42e1ed6
marshall chen <marshallchan666@outlook.com>
2021-06-12 18:35:30 +0800

    router jobs

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Jobs from '../views/jobs/Jobs.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
 
 const routes = [
   {
@@ -15,6 +17,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: About
+  },
+  {
+    path: '/jobs',
+    name: Jobs,
+    component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: JobDetails,
+    component: JobDetails
   }
 ]
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi, voluptatum. Temporibus deleniti distinctio laudantium delectus voluptatibus fugit, repellendus fugiat facilis autem nostrum praesentium culpa vel. Accusantium maiores doloribus eveniet in?</p>
   </div>
 </template>
